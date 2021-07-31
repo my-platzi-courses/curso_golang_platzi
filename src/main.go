ac7614610d97aacc9de871860fa738c8adbeb039
Juan <jd.camayo@gmail.com>
2021-07-31 11:16:26 -0500

    arithmetic operators

--- src/main.go
+++ src/main.go
@@ -29,4 +29,39 @@ func main() {
 	const baseCuadrado = 10
 	areaCuadrado := baseCuadrado * baseCuadrado
 	fmt.Println("El area del cuadrado: ", areaCuadrado)
+
+	x := 10
+	y := 50
+
+	// Suma
+	result := x + y
+	fmt.Println("Suma: ", result)
+
+	// Resta 
+	result = y - x
+	fmt.Println("Resta: ", result)
+
+	// Multiplicación
+	result = x * y
+	fmt.Println("Multiplicación: ", result)
+
+	// División
+	result = y / x
+	fmt.Println("División: ", result)
+
+	// Modulo
+	result = y % x
+	fmt.Println("Modulo: ", result)
+
+	// Incremental
+	x++
+	fmt.Println("Incremental: ", x)
+
+	// Decremental
+	x--
+	fmt.Println("Decremental: ", x)
+
+	// Retos
+	// - Rectangulo, trapecio y de un círculo
+	
 }
